9058c760aca6615541e78dd994e0d5e0d4fd99c2
Edenilson Avila <avilaedenilson86@gmail.com>
2024-02-23 14:55:54 -0600

    agregando el modelo de categorias y cambiando una propiedad en la modelo de equipos

--- WebApplication1/Controllers/equiposController.cs
+++ WebApplication1/Controllers/equiposController.cs
@@ -39,6 +39,33 @@ namespace WebApplication1.Controllers
             return Ok(listadoEquipo);
         }
 
+
+
+
+
+        /// <summary>
+        /// EndPoint que retorna el lisado de todos los equipos existentes 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                                           where e.id_equipos == id
+                                           select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+
+
+
     }
 
 
---
+++ WebApplication1/models/Categorias.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.models
+{
+    public class Categorias
+    {
+    }
+}
