8f798b9f5782c16a053f3fc91054d182cb811b74
nesto <nesto@LAPTOP-P4L0CKO8>
2024-02-23 12:19:14 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/equiposController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+
+        private readonly equiposContext _equiposContexto;
+
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+
+
+        }
+
+        /// <summary>
+        /// EndPoint que retorna el lisado de todos los equipos existentes 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count() == 0)
+            {
+                return NotFound();
+            }
+            return Ok(listadoEquipo);
+        }
+
+    }
+
+
+}
